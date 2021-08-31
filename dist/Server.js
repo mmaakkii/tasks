de533fb6374d6ae8a6892ed1ec469718ed059d21
Makki <makkii.dev@gmail.com>
2021-08-31 19:38:32 +0530

    added cors .

--- dist/Server.js
+++ dist/Server.js
@@ -7,6 +7,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const path_1 = __importDefault(require("path"));
 const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const http_status_codes_1 = __importDefault(require("http-status-codes"));
 require("express-async-errors");
@@ -21,6 +22,7 @@ const { BAD_REQUEST } = http_status_codes_1.default;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(cookie_parser_1.default());
+app.use(cors_1.default());
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan_1.default('dev'));
